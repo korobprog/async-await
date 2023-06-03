16a815af2eaf62ddcc2ac89c93e57e1f605b62bf
Maxim Korobkov <62425861+korobprog@users.noreply.github.com>
2023-06-04 09:58:13 +1000

    no commit

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -4,11 +4,10 @@ import read from "./reader";
 export default class GameSavingLoader {
   static async load() {
     try {
-      const buffer = await read(); // get the data buffer from the read() function
-      const rawString = await json(buffer); // convert the buffer to text and parse it as json
-      const data = JSON.parse(rawString); // extract the needed fields from the json object
+      const buffer = await read();
+      const rawString = await json(buffer);
+      const data = JSON.parse(rawString);
 
-      // return a new GameSaving object with the parsed fields
       const gameSaving = {
         id: data.id,
         created: new Date(data.created),
--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,8 @@ import GameSavingLoader from "./GameSavingLoader";
 
 GameSavingLoader.load()
   .then((saving) => {
-    console.log(saving); // do something with the GameSaving object
+    console.log(saving);
   })
   .catch((error) => {
-    console.log(error); // handle the error if something goes wrong
+    console.log(error);
   });
